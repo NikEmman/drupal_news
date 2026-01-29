b0cf753e0d1b9ce935dba1f749e24ccf93f6f130
NikEmman <121699424+NikEmman@users.noreply.github.com>
2026-01-29 10:37:17 +0200

    Add Bario theme

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '2ceee553c39a4a9a';
+  public const string VERSIONS_HASH = '44edf014430cd714';
 
 }
